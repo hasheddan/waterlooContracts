24ad2195da4695e48fcaa5ac63a092a9ecc2c730
Deon <deonsummers91@gmail.com>
2017-10-14 07:07:14 -0500

    update zerox

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var Web3 = require('web3');
 var ZeroEx = require('0x.js').ZeroEx
+var BigNumber = require('bignumber.js');
 
 var provider = new Web3.providers.HttpProvider('http://localhost:8545');
 
@@ -22,6 +23,9 @@ app.get('/test', function(req, res) {
     });
 });
 
+
+app.get('zerox', runThoughEverything);
+
 app.listen(app.get("port"), function () {
   console.log('Server running on http://localhost:' + app.get("port"))
 })
@@ -31,3 +35,35 @@ app.listen(app.get("port"), function () {
 	Make npm start run the 0x testrpc commands as well
 
 */
+
+// Goes through the whole zerox stuff
+async function runThoughEverything(req, res) {
+    await zeroEx.setProviderAsync(provider);
+    let date = new Date();
+    try {
+        var availableAddresses = await zeroEx.exchange.getContractAddressAsync(); 
+        let form = {
+            "maker": web3.eth.accounts[0],
+            "taker": web3.eth.accounts[1],
+            "takerTokenAddress": await zeroEx.exchange.getZRXTokenAddressAsync(),
+            "makerTokenAddress": await zeroEx.exchange.getZRXTokenAddressAsync(),
+            "makerTokenAmount": new BigNumber("2"),
+            "takerTok+enAmount": new BigNumber("2"),
+            "takerFee": new BigNumber("0"),
+            "makerFee": new BigNumber("0"),
+            "exchangeContractAddress": availableAddresses,
+            "feeRecipient": web3.eth.accounts[0],
+            "expirationUnixTimestampSec": new BigNumber(''+Math.floor(date/1000)+100000),
+            "salt": ZeroEx.generatePseudoRandomSalt()
+        }
+        let hash = ZeroEx.getOrderHashHex(form);
+        let success = await zeroEx.signOrderHashAsync(hash, web3.eth.accounts[0])
+        form.ecSignature = success;
+        let number = new BigNumber(1);
+        // let tx = await zeroEx.exchange.validateFillOrderThrowIfInvalidAsync(form, number, web3.eth.accounts[0])
+        let tx = await zeroEx.exchange.fillOrderAsync(form, number, true, web3.eth.accounts[1]);
+        console.log(tx);
+    } catch (error) {
+        console.log(error);
+    }
+}
