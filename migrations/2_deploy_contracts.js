548e8b73df32322c968d2ed86e6d3572a49f4f48
HashedDan <dmangum@hashedhealth.com>
2017-10-14 00:20:34 -0400

    updates

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+const HumanStandardToken = artifacts.require(`./HumanStandardToken.sol`)
 
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
-};
+module.exports = (deployer) => {
+  deployer.deploy(HumanStandardToken)
+}
